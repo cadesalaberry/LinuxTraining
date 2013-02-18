f530168e34938e89db6082616169b6ecd1166ecf
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-02-18 03:38:59 -0500

    Question 2 working.

--- hw2/q1.h
+++ hw2/q1.h
@@ -1,29 +1,62 @@
 #ifndef Q1_H
 #define Q1_H
 
-int pascalTriangle[100][100];
+/**
+ * Make sure to synchronise the values of Y_MAX and X_MAX.
+ * The program had a weird behaviour when Y_MAX=X_MAX, so I had to
+ * set X_MAX to a different value.
+ * (note that a lower value works as well)
+ */ 
+#define Y_MAX 100
+#define X_MAX 101
 
+int pascalTriangle[Y_MAX][X_MAX];
+int GetPTElement(int level, int eleIdx);
 
 /**
- * in this function your supposed to build the pascal triangle, (filling element values to the global variable pascalTriangle
+ * in this function your supposed to build the pascal triangle,
+ * (filling element values to the global variable pascalTriangle
  * notice that you have to fill all 100 rows
  */
 void buildPascalTriangle()
 {
-	//TODO: fill your code here	
+	// Builds the first line.
+	pascalTriangle[0][0] = 1;
+	int level, index;
+
+	for (level = 1; level <= Y_MAX; level++)
+	{
+		for (index = 0; index <= level && index <= X_MAX; index++)
+		{
+			int left = GetPTElement(level-1, index-1);
+			// If the element does not exist, replace by zero.
+			if (left < 0) left = 0;
+
+			int right = GetPTElement(level-1, index);
+			// If the element does not exist, replace by zero.
+			if (right < 0) right = 0;
+			
+			pascalTriangle[level][index] = left + right;
+		}
+		//printf("\n");
+	}
 }
 
 /**
- * in this function, you are supposed to return the element specified by level, and eleidx, 
+ * in this function, you are supposed to return the element
+ * specified by level, and eleidx, 
  *
  * e.g. GetPTElement(2, 1) will return 2
  * if (level, eleIdx) is referring to a 'non-exist' element, return -1;
  */
 int GetPTElement(int level, int eleIdx)
 {
-	int ret = 0;
-	//TODO: fill your code here
-	return ret;
+	if (0 <= level && level <= Y_MAX) {
+		if (0 <= eleIdx && eleIdx <= level && eleIdx <= X_MAX) {
+			
+			return pascalTriangle[level][eleIdx];
+		}
+	}
+	return -1;
 }
-
 #endif
--- hw2/q2.h
+++ hw2/q2.h
@@ -1,6 +1,6 @@
 #ifndef Q2_H
 #define Q2_H
-
+#include <string.h>
 /**
  * convert binary representation of integer to decimal representation
  * in this function you're suppose to do convert binary to decimal
@@ -9,8 +9,14 @@
  */
 int binTodec(const char *binstr)
 {
-	int ret = 0;
-	//TODO: fill your code here
+	int ret = 0, i, lenght = strlen(binstr);
+	
+	// Makes sure the program does not go past 10 digits.
+	for(i = 0; i < lenght && i < 10; i++)
+    {
+		// Transforms the char into an int
+		ret = ret * 2 + (binstr[i] - '0');
+    }
 	return ret;
 }
 #endif
