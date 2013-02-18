2048945d84e45a268d6001c51e15bf57e445e5a0
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-02-18 11:23:51 -0500

    Started working on Q5.

--- hw2/q5.h
+++ hw2/q5.h
@@ -6,12 +6,43 @@
  * input: input string
  * return the reversed string
  */
+
+/**
+ * Swapes the position of 2 letters.
+ */ 
+void swap(char *first,char *second)
+{
+	char temp;
+	while(first < second)
+	{
+		temp = *first;
+		*first++ = *second;
+		*second-- = temp;
+	}
+}
+
 char *reverse(char const *input)
 {
 	char *ret = (char *)malloc(sizeof(char) * strlen(input));
 	//TODO:preprocess
 	//the input may end with a \n, so you have to strip that
 	
+	int size = strlen(ret)-1;
+	int cursor=0;
+	while(cursor < size) {
+		if (input[cursor] == '\n') {
+			ret[cursor] = ' ';
+		} else {
+			ret[cursor] = input[cursor];
+		}
+	}
+	
+	int i, j;
+	for (i = size, j=0 ; 0 <= i, j < size; i--, j++) {
+		swap(&ret[i], &ret[j]);
+	}
+	
+	printf("%s", ret);
 	//TODO:reverse input, and save the result in ret
 	
 	return ret;
